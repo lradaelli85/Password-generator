cdfe36a5c2c524092a3c780258ba8f384095989a
luca <l.radaelli85@noreply.github.com>
2019-02-27 14:28:59 +0100

    fix delay with long password

--- passwordgenerator.sh
+++ passwordgenerator.sh
@@ -3,4 +3,18 @@
 #Characters should not be repeated
 #16 passwords are printed,4 password for each line
 
-cat /dev/urandom |tr -dc 'a-zA-Z0-9-_!.@#$%&?=-' |fold -w 16 |grep -vi '\(.\).*\1' |head -n 16 |xargs -n 4
+function usage(){
+  echo "Usage: $0 <password-lenght>"
+}
+
+if [ "$#" -eq 1 ]
+  then
+    if [ "$1" -le 20 ]
+      then
+        cat /dev/urandom |tr -dc 'a-zA-Z0-9-!@#$%^&*_+=-' |fold -w $1 |grep -vi '\(.\).*\1' |head -n 16 |xargs -n 4
+      else
+        cat /dev/urandom |tr -dc 'a-zA-Z0-9-!@#$%^&*_+=-' |fold -w $1 |head -n 16 |xargs -n 4
+    fi
+  else
+    usage
+fi
