89fcdd92c4d33efd8e462a2ad6f5e76c7ead99a6
luca <l.radaelli85@noreply.github.com>
2019-07-05 14:36:02 +0200

    added some extra chars

--- passwordgenerator.sh
+++ passwordgenerator.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-#It creates a password 16 chars long that contains numbers,letters(upper/lower case) and symbols(_!.@#$%&?=-) .
-#Characters should not be repeated if password is less then 21 char long
+#!/usr/bin/env bash
+#It creates a passwords that contain numbers,letters(upper/lower case) and symbols.
+
 function usage(){
   echo "Usage: $0 <password-lenght>"
 }
@@ -10,15 +10,13 @@ if [ "$#" -eq 1 ]
     if [ "$1" -lt 21 ]
           then
                XARGSN="4"
-			   NO_REPEAT="| grep -vi "'\(.\).*\1'" "
-			   #echo ${NO_REPEAT}
     elif [ "$1" -gt 20 ] && [ "$1" -lt 65 ]
 	  then
 	       XARGSN="2"
     else
 	  XARGSN="1"
     fi	  
-    eval "cat /dev/urandom |tr -dc 'a-zA-Z0-9-!@#$%^&*_+=-' |fold -w $1 ${NO_REPEAT} |head -n 16 | xargs -n $XARGSN"
+	cat /dev/urandom |tr -dc 'A-Za-z0-9!#$%&()*+,-./:;<=>?@[\]^_{|}~' |fold -w $1 |head -n 16 | xargs -n $XARGSN
   else
     usage
 fi
